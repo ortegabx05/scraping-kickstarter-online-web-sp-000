43abe13c63c46081dcfd6f05c9d7b31680a530ef
jessi ortega <ortegabx05@gmail.com>
2019-08-14 22:24:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,3 +10,11 @@ kickstarter = Nokogiri::HTML(html)
 def create_project_hash
   # write your code here
 end
+
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
+end
+ 
+create_project_hash
